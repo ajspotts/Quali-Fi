22b2881e00be5fd29aaab518a0615f78cfcd9d03
Alexander <ajspottswood@gmail.com>
2016-11-02 22:56:03 -0400

    Added applicants

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ angular
   .controller("OpeningNewController", ["OpeningFactory", "$state", OpeningNewControllerFunction])
   .controller("OpeningEditController", ["OpeningFactory", "$state", "$stateParams", OpeningEditControllerFunction])
   .factory( "OpeningFactory", ["$resource", OpeningFactoryFunction])
+  .controller("ApplicantIndexController", ["ApplicantFactory", ApplicantIndexControllerFunction])
+  .controller("ApplicantShowController", ["ApplicantFactory", "$stateParams", ApplicantShowControllerFunction])
+  .controller("ApplicantNewController", ["ApplicantFactory", "$state", ApplicantNewControllerFunction])
+  .controller("ApplicantEditController", ["ApplicantFactory", "$state", "$stateParams", ApplicantEditControllerFunction])
+  .factory( "ApplicantFactory", ["$resource", ApplicantFactoryFunction])
 
 function RouterFunction($stateProvider){
   $stateProvider
@@ -36,6 +41,30 @@ function RouterFunction($stateProvider){
     controller: "OpeningEditController",
     controllerAs: "vm",
     templateUrl: "js/ng-views/openings/edit.html"
+  })
+  .state("applicantIndex", {
+    url: "/applicants",
+    controller: "ApplicantIndexController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/applicants/index.html"
+  })
+  .state("applicantNew", {
+    url: "/applicants/new",
+    templateUrl: "js/ng-views/applicants/new.html",
+    controller: "ApplicantNewController",
+    controllerAs: "vm"
+  })
+  .state("applicantShow", {
+    url: "/applicants/:id",
+    controller: "ApplicantShowController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/applicants/show.html"
+  })
+  .state("applicantEdit", {
+    url: "/applicants/:id/edit",
+    controller: "ApplicantEditController",
+    controllerAs: "vm",
+    templateUrl: "js/ng-views/applicants/edit.html"
   })
 }
 
@@ -79,3 +108,44 @@ function OpeningShowControllerFunction(OpeningFactory, $stateParams){
 
 
  }
+
+ function ApplicantFactoryFunction($resource){
+   return $resource( "https://quali-fi.herokuapp.com/applicants/:id", {}, {
+     update: {method: "PUT"}
+   })
+ }
+
+ function ApplicantIndexControllerFunction(ApplicantFactory) {
+   this.applicants = ApplicantFactory.query()
+ }
+
+ function ApplicantNewControllerFunction(ApplicantFactory, $state){
+      this.applicant = new ApplicantFactory()
+      this.create = function(){
+        this.applicant.$save().then(response => $state.go("applicantIndex"))
+          // this is where the redirect should happen
+        }
+      }
+
+
+ function ApplicantEditControllerFunction(ApplicantFactory, $state, $stateParams){
+   this.applicant = ApplicantFactory.get({id: $stateParams.id})
+   this.update = function(){
+     this.applicant.$update({id: $stateParams.id}).then(response => $state.go('applicantIndex'))
+
+   }
+   this.destroy = function(){
+       this.applicant.$delete({id: $stateParams.id}).then(response => $state.go('applicantIndex'))
+     }
+ }
+
+ function ApplicantShowControllerFunction(ApplicantFactory, $stateParams){
+   // To access the full applicant object
+   // this.applicant = ApplicantFactory.get({id: $stateParams.id})
+   // console.log(this.applicant)
+
+   ApplicantFactory.get({id: $stateParams.id}).$promise.then(response => this.applicantName = this.applicant = response.name)
+
+
+
+  }
